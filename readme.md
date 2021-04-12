85f0f3cd9d5fe3f647c9a13685d89c356282b850
keith <keith@webizly.com>
2021-04-12 18:38:36 -0400

    updated readme

--- readme.md
+++ readme.md
@@ -1,3 +1,28 @@
+# This will help you understand images on the web in terms of file format, using CSS, and how to optimize your workflow with webpack
+## Your Assignment
+Part 1:
+Setup webpack to optimize your images automaticly.
+
+Part 2:
+Make an F and Z layout using CSS grid and create a sidebar with image content.  Don't add text to it as we will do this in the next lesson where we work on positioning.  
+
+
+## Topic 1 - Image File Formats
+Refer to this page for a comprehensive list of image formats and their purpose [Click Here](https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types)
+##  Topic 2 - Cropping with a free tool and Saving
+Pixlr is free and web based [Click Here](https://pixlr.com/)
+## Topic 3 - Manipulating Images with CSS
+Refer to this page for a comprehensive list of ways to manipulate images with CSS alone [Click Here](https://www.w3schools.com/css/css3_images.asp)
+## Topic 4 - Automating your image workflow with Webpack
+You can see a basic implementation of the webpack to compress images here:[click here](https://webpack.js.org/plugins/image-minimizer-webpack-plugin/)
+The big trick to optimizing jpg can be found on line 44 of my webpack.config file.  The line says "['mozjpeg', { quality: 50 }]," and this is telling the mozjpeg plugin to reduce the quality of jpeg by 50%.  Depending on the quality of the image this could be ok or it could be a problem because this is a lossy compression format and will take low quality starting images and make them unusable.
+## Lighthouse - Used for Finding Issues in Chrome
+https://developers.google.com/web/tools/lighthouse#devtools
+##  CSS Grid Guide
+https://css-tricks.com/snippets/css/complete-guide-grid/
+### Grid Layout Tutorial https://www.youtube.com/watch?v=jV8B24rSN5o
+
+# Install Instructions from previous lesson
 Prerequisites:
 Install Node:
 https://nodejs.org/en/download/
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     // Set the mode to development or production
     mode: 'development',
     watch: true,
-
     // Control how source maps are generated
     devtool: 'inline-source-map',
-
     entry: {
         index: './src/js/index.js',
     },
@@ -17,14 +16,47 @@ module.exports = {
         open: true,
         compress: true,
         port: 8080,
-
     },
     output: {
         filename: 'js/bundle.js',
         path: path.resolve(__dirname, 'docs'),
         clean: true,
     },
+    module: {
+        rules: [
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    type: 'asset',
+                    enforce: 'pre',
+                    use: {
+                        loader: 'url-loader',
+                    },
+                },
+        ],
+    },
     plugins: [
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['mozjpeg', { quality: 50 }],
+                    ['jpegtran', { progressive: true,quality: [0.5, 0.5] }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    [
+                        'svgo',
+                        {
+                            plugins: [
+                                {
+                                    removeViewBox: false,
+                                },
+                            ],
+                        },
+                    ],
+                ],
+            },
+        }),
         new CopyPlugin({
             patterns: [
                 { from: "src",
